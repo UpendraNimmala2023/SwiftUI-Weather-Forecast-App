b1b524555a131c0a42ea81bda48f827445d0f10e
UpendraNimmala2023 <135559739+UpendraNimmala2023@users.noreply.github.com>
2023-10-26 21:59:23 -0400

    Initial Commit
    Initial Commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Upendra Nimmala on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
